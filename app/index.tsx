e37f566b76fa65e2eaaf61306ca1dec0da3bdcf0
Dilwith93 <Dileepajw@gmail.com>
2025-09-07 14:26:07 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}
+ >  <Text>Edit app/index.tsx to edit this screen.Dileepa </Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+   },
+   content: {
+    fontSize: 29,
+    color: "white",
+    }
+  });
